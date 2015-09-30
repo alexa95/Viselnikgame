09435157023488d5dd38baec5bcadc4274983bde
KalitaAleks <kalita.alieksandr@mail.ru>
2015-09-30 16:10:50 +0600

    Creat Forms
    Создание всех форм и переходов между ними

---
+++ ViselnikGame/Form3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViselnikGame
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        public String s;
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+             s = "категория1";
+            Form4 f4 = new Form4();
+            this.Hide();
+            f4.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s = "категория1";
+            Form4 f4 = new Form4();
+            this.Hide();
+            f4.Show();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            s = "категория1";
+            Form4 f4 = new Form4();
+            this.Hide();
+            f4.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            this.Hide();
+            f2.Show();
+        }
+    }
+}
